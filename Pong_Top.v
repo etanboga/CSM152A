9778c6622461772ec944f60f328d3c69601d0ad7
etanboga <ege72282@gmail.com>
2019-03-05 15:53:39 -0800

    Add files via upload
    joystick files

---
+++ Pong_Top.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:59:18 03/05/2019 
+// Design Name: 
+// Module Name:    Pong_Top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Pong_Top(
+    clk,
+    rst,
+    led,
+    miso,
+    mosi,
+    ss, //slave select for spi?
+    sclk //serial clock for spi communication
+
+
+
+    );
+
+
+    //---INPUTS OUTPUTS----
+    input clk;
+    input rst;
+    input miso;					// Master In Slave Out, Pin 3, Port JA
+    
+    //input [2:0] SW;			// Switches 2, 1, and 0
+    output led[3:0];
+    output SS;					// Slave Select, Pin 1, Port JA
+    output MOSI;				// Master Out Slave In, Pin 2, Port JA
+    output SCLK;				// Serial Clock, Pin 4, Port JA
+    
+    
+    
+    //--------SUBMODULE DECLARATIONS-----------------
+    
+    
+    
+    
+    
+endmodule
---
+++ joystick.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:27:16 03/05/2019 
+// Design Name: 
+// Module Name:    joystick 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module joystick(
+    clk,
+    rst,
+    miso,
+    ss,
+    mosi,
+    sclk,
+    positionOutput
+    );
+    
+    input clk;
+    input rst;
+    input miso;
+    output ss;
+    output mosi;
+    output sclk;
+    output [3:0] positionOutput; //up down left right
+
+    wire ss; //spi?
+    wire mosi;
+    wire sclk; //not sure what clk's for
+
+    wire [7:0] sndData;
+    
+    
+    // Signal to send/receive data to/from PmodJSTK
+    wire sndRec;
+
+    // Data read from PmodJSTK
+    wire [39:0] jstkData;
+
+    // Signal carrying output data that user selected
+    wire [9:0] posData;
+    
+            
+            
+            
+            //-----------------------------------------------
+			//  	  			PmodJSTK Interface
+			//-----------------------------------------------
+			PmodJSTK PmodJSTK_Int(
+					.CLK(clk),
+					.RST(rst),
+					.sndRec(sndRec),
+					.DIN(sndData),
+					.MISO(miso),
+					.SS(ss),
+					.SCLK(sclk),
+					.MOSI(mosi),
+					.DOUT(jstkData)
+			);
+            
+
+			
+            
+      //----------IMPLEMENTATION--------------------------
+    //up down
+    
+    //450 = 0111000010
+    //520=1000001000
+    //up down left right
+    assign positionOutput[3] = {jstkData[25:24], jstkData[39:32]} > 10'b1000001000; //arbitrary threshold 450-520
+    assign positionOutput[2] = {jstkData[25:24], jstkData[39:32]} <= 10'b0111000010;
+    //left right
+    assign positionOutput[3] = {jstkData[9:8], jstkData[23:16]} > 10'b1000001000;
+    assign positionOutput[2] = {jstkData[9:8], jstkData[23:16]} <= 10'b0111000010;
+    
+    
+
+
+endmodule
